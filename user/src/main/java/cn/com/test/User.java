33e5659ca54999105ffdf730b203b6e5cfdaf4eb
System Administrator <root@yanhaijiedeMacBook-Pro.local>
2017-06-21 06:58:57 +0800

    first commit

---
+++ user/src/main/java/cn/com/test/User.java
@@ -0,0 +1,10 @@
+package cn.com.test;
+
+/**
+ * Created by yanhaijie on 2017/6/21.
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.print("终于建立成功了");
+    }
+}
