7f462da730be6e304cbc4af4e77485c63410544e
yanhaijie <237510566@qq.com>
2017-06-21 19:10:44 +0800

    第三次 提交

--- user/src/main/java/cn/com/controller/UserLoginController.java
+++ user/src/main/java/cn/com/controller/UserLoginController.java
@@ -3,6 +3,8 @@ package cn.com.controller;
 
 public class UserLoginController {
     public static void main(String[] args) {
+
         System.out.print("第一个controller");
+        System.out.print("真他妈费劲啊。全是英语");
     }
 }
