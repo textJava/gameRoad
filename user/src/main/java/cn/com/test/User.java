a07dc7d8c58fbc50559e0146aa5b1db085fec5d0
yanhaijie <237510566@qq.com>
2017-06-21 18:57:35 +0800

    第二次 提交

--- user/src/main/java/cn/com/test/User.java
+++ user/src/main/java/cn/com/test/User.java
@@ -5,6 +5,6 @@ public class User {
     public static void main(String[] args) {
         System.out.print("终于建立成功了");
         System.out.print("环境弄得真的好费劲啊");
-        System.out.print("忘了push了");
+        System.out.print("忘了push了。");
     }
 }
