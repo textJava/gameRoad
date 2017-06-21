03362187f6d376436e5821291034e2bcefce7ec9
yanhaijie <237510566@qq.com>
2017-06-21 19:24:33 +0800

    第六次 提交

--- user/src/main/java/cn/com/controller/UserLoginController.java
+++ user/src/main/java/cn/com/controller/UserLoginController.java
@@ -7,7 +7,7 @@ public class UserLoginController {
         System.out.print("第一个controller");
         System.out.print("真他妈费劲啊。全是英语");
         for (int a=0;a<100;a++){
-            System.out.print("中文a ");
+            System.out.print("中文ab");
         }
     }
 }
