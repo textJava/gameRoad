f51de8687bf99aa1c818c06afdb1e3d7dd5c666c
yanhaijie <237510566@qq.com>
2017-06-21 18:43:26 +0800

    第二次 提交

--- user/src/main/java/cn/com/test/User.java
+++ user/src/main/java/cn/com/test/User.java
@@ -1,10 +1,9 @@
 package cn.com.test;
 
-/**
- * Created by yanhaijie on 2017/6/21.
- */
+
 public class User {
     public static void main(String[] args) {
         System.out.print("终于建立成功了");
+        System.out.print("环境弄得真的好费劲啊");
     }
 }
