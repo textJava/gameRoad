1901f1fb5d0eaee495e80f35053e8fe8315957e0
Yhaijie <18518081113@163.com>
2017-10-13 15:39:15 +0800

    12

---
+++ core/src/main/java/com/yhj/test/TestDate.java
@@ -0,0 +1,14 @@
+package com.yhj.test;
+
+public class TestDate {
+
+	public static void main(String[] args) {
+		gets();
+	}
+	/**
+	 * 获取秒
+	 */
+	public static long gets(){
+		return System.currentTimeMillis();
+	}
+}
