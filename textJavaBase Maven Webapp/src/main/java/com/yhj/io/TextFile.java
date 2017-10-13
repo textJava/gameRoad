eadb11ab21a12fe95b03bd8b3819699b704ef418
Yhaijie <18518081113@163.com>
2017-10-13 15:35:54 +0800

    1

--- textJavaBase Maven Webapp/src/main/java/com/yhj/io/TextFile.java
+++ textJavaBase Maven Webapp/src/main/java/com/yhj/io/TextFile.java
@@ -1,5 +1,11 @@
 package com.yhj.io;
 
+import com.yhj.test.TestDate;
+
 public class TextFile {
 
+	public static void main(String[] args) {
+		long ss=TestDate.gets();
+		System.out.println(ss);
+	}
 }
